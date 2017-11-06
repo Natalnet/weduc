19916654ea96115a8f2b3e2b9ff5936dfd1cde64
Francinaldo Almeida <almeidafranci@users.noreply.github.com>
2017-11-06 16:00:42 -0300

    Track default files

---
+++ storage/app/default_files/WeducClient.java
@@ -0,0 +1,94 @@
+import javax.swing.JOptionPane;
+import java.io.*;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class WeducClient {
+    public static void main(String[] args)  throws IOException {
+        File dir = new File("Arquivos_Weduc/$LINGUAGEM");
+        if(!dir.isDirectory()) {
+            // attempt to create the directory here
+            boolean successful = dir.mkdirs();
+            if (successful)
+            {
+                // creating the directory succeeded
+                System.out.println("directory was created successfully");
+
+                System.setProperty("user.dir", System.getProperty("user.dir")+"/Arquivos_Weduc/$LINGUAGEM");
+
+                String fileURL = "$LANGUAGE_FILES_URL";
+                String saveDir = System.getProperty("user.dir");
+                try {
+                    HttpDownloadUtility.downloadFile(fileURL, saveDir);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+
+                String fileZip = System.getProperty("user.dir") + "/$LINGUAGEM.zip";
+                byte[] buffer = new byte[1024];
+                ZipInputStream zis = new ZipInputStream(new FileInputStream(fileZip));
+                ZipEntry zipEntry = zis.getNextEntry();
+                while(zipEntry != null){
+                    String fileName = System.getProperty("user.dir") + "/" + zipEntry.getName();
+                    File newFile = new File(fileName);
+
+                    if (zipEntry.isDirectory()) {
+                        newFile.mkdirs();
+                        zipEntry = zis.getNextEntry();
+                        continue;
+                    }
+
+                    FileOutputStream fos = new FileOutputStream(newFile);
+                    int len;
+                    while ((len = zis.read(buffer)) > 0) {
+                        fos.write(buffer, 0, len);
+                    }
+                    fos.close();
+                    zipEntry = zis.getNextEntry();
+                }
+                zis.closeEntry();
+                zis.close();
+
+                File tempZip = new File(fileZip);
+                tempZip.delete();
+            } else {
+                // creating the directory failed
+                System.out.println("failed trying to create the directory");
+            }
+        } else {
+            System.setProperty("user.dir", System.getProperty("user.dir")+"/Arquivos_Weduc/$LINGUAGEM");
+            System.out.println("im here: " +System.getProperty("user.dir"));
+        }
+
+        String fileURL = "$DOWNLOAD_URL";
+        String saveDir = System.getProperty("user.dir");
+        try {
+            HttpDownloadUtility.downloadFile(fileURL, saveDir);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+
+
+        String comando = "$SEND_CODE";
+        comando = comando.replace("diretorio", System.getProperty("user.dir"));
+        if (comando.contains("porta")) {
+            String portName = (String)JOptionPane.showInputDialog(null, "Selecione a porta em que seu dispositivo esta conectado:", "W-Educ - Seletor de Portas",JOptionPane.QUESTION_MESSAGE, null,SerialPortList.getPortNames(),null);
+
+            if (portName != null){
+                comando = comando.replace("porta",  portName);
+            }
+        }
+
+        String[] comandoSeparado = comando.split(" ");
+        try {
+            Process proc = new ProcessBuilder().command(comandoSeparado)
+                                               .inheritIO()
+                                               .start();
+            int exitCode = proc.waitFor();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
