b23948f4890f7ea4a2ea5af9d6e31b7b8936cefb
Francinaldo Almeida <almeidafranci@users.noreply.github.com>
2017-11-09 07:23:22 -0500

    Localize login and register

---
+++ resources/lang/pt-BR/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'As credenciais informadas não conferem com nossos registros.',
+    'throttle' => 'Tentativas de login em excesso. Por favor tente novamente em :seconds segundos.',
+
+];
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,42 +10,47 @@
                 <form class="form-horizontal" method="POST" action="{{ route('register') }}">
                     {{ csrf_field() }}
 
-                    <div class="input-group mb-3">
-                        <span class="input-group-addon"><i class="icon-user"></i></span>
-                        <input type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" placeholder="Username">
-
+                    <div class="mb-3">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="icon-user"></i></span>
+                            <input type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" placeholder="Username">
+                        </div>
                         @if ($errors->has('name'))
-                            <span class="help-block">
+                            <div class="invalid-feedback" style="display: block;">
                                 <strong>{{ $errors->first('name') }}</strong>
-                            </span>
+                            </div>
                         @endif
                     </div>
 
-                    <div class="input-group mb-3">
-                        <span class="input-group-addon"><i class="fa fa-envelope-o"></i></span>
-                        <input type="text" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" placeholder="E-mail">
-
+                    <div class="mb-3">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-envelope-o"></i></span>
+                            <input type="text" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" placeholder="E-mail">
+                        </div>
                         @if ($errors->has('email'))
-                            <span class="help-block">
+                            <div class="invalid-feedback" style="display: block;">
                                 <strong>{{ $errors->first('email') }}</strong>
-                            </span>
+                            </div>
                         @endif
                     </div>
 
-                    <div class="input-group mb-3">
-                        <span class="input-group-addon"><i class="fa fa-lock"></i></span>
-                        <input type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" value="{{ old('password') }}" placeholder="@lang('register.form.password')">
-
+                    <div class="mb-3">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                            <input type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" value="{{ old('password') }}" placeholder="@lang('register.form.password')">
+                        </div>
                         @if ($errors->has('password'))
-                            <span class="help-block">
+                            <div class="invalid-feedback" style="display: block;">
                                 <strong>{{ $errors->first('password') }}</strong>
-                            </span>
+                            </div>
                         @endif
                     </div>
 
-                    <div class="input-group mb-3">
-                        <span class="input-group-addon"><i class="fa fa-lock"></i></span>
-                        <input type="password" class="form-control" name="password_confirmation" placeholder="@lang('register.form.password_confirmation')">
+                    <div class="mb-3">
+                        <div class="input-group mb-3">
+                            <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                            <input type="password" class="form-control" name="password_confirmation" placeholder="@lang('register.form.password_confirmation')">
+                        </div>
                     </div>
 
                     <button type="submit" class="btn btn-block btn-primary">
