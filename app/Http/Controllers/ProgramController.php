8a7ba5a435b265fedc6bd82f5cc46b9b802a7760
Francinaldo Almeida <almeidafranci@users.noreply.github.com>
2018-08-31 19:09:23 -0300

    Include middleware

--- app/Http/Controllers/ProgramController.php
+++ app/Http/Controllers/ProgramController.php
@@ -14,6 +14,16 @@ use Natalnet\Relex\Types;
 
 class ProgramController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function program()
     {
         // return ProgrammingLanguage::pluck('name', 'id')->toJson();
