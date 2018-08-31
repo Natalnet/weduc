7e62dce6a04c77419fe1d05726f438d188ae9930
Francinaldo Almeida <almeidafranci@users.noreply.github.com>
2018-08-31 11:51:37 -0300

    Localize metrics

--- app/Metrics/CompilationsPerDay.php
+++ app/Metrics/CompilationsPerDay.php
@@ -3,11 +3,50 @@
 namespace App\Metrics;
 
 use App\CompilationLog;
+use Cake\Chronos\Chronos;
 use Illuminate\Http\Request;
 use App\Metrics\Core\Trend;
 
 class CompilationsPerDay extends Trend
 {
+    /**
+     * Format the aggregate result date into a proper string.
+     *
+     * @param  string  $result
+     * @param  string  $unit
+     * @param  bool  $twelveHourTime
+     * @return string
+     */
+    protected function formatAggregateResultDate($result, $unit, $twelveHourTime)
+    {
+        switch ($unit) {
+            case 'month':
+                return $this->formatAggregateMonthDate($result);
+
+            case 'week':
+                return $this->formatAggregateWeekDate($result);
+
+            case 'day':
+                return with(Chronos::createFromFormat('Y-m-d', $result), function ($date) {
+                    return $date->format('j').' de '.__($date->format('F')).' de '.$date->format('Y');
+                });
+
+            case 'hour':
+                return with(Chronos::createFromFormat('Y-m-d H:00', $result), function ($date) use ($twelveHourTime) {
+                    return $twelveHourTime
+                        ? $date->format('j').' de '.__($date->format('F')).' - '.$date->format('g:00 A')
+                        : $date->format('j').' de '.__($date->format('F')).' - '.$date->format('G:00');
+                });
+
+            case 'minute':
+                return with(Chronos::createFromFormat('Y-m-d H:i:00', $result), function ($date) use ($twelveHourTime) {
+                    return $twelveHourTime
+                        ? $date->format('j').' de '.__($date->format('F')).' - '.$date->format('g:i A')
+                        : $date->format('j').' de '.__($date->format('F')).' - '.$date->format('G:i');
+                });
+        }
+    }
+
     /**
      * Calculate the value of the metric.
      *
