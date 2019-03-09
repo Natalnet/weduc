d0055097ef6d627102dd57a430e740c4153df260
Francinaldo Almeida <almeidafranci@users.noreply.github.com>
2019-03-08 22:32:31 -0300

    Include OBR Simulada

---
+++ config/obrsimulada.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | OBR Simulada Path
+    |--------------------------------------------------------------------------
+    |
+    | This is the URI path where OBR Simulada will be accessible from. Feel free
+    | to change this path to anything you like. Note that the URI will not
+    | affect the paths of its internal API that aren't exposed to users.
+    |
+    */
+
+    'path' => 'obrsimulada',
+];
