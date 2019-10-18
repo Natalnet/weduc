0020a9ded693fbf08ed8856d8c8620898dc1ceb2
Francinaldo Almeida <almeidafranci@users.noreply.github.com>
2019-10-18 11:52:28 -0300

    Change middleware

--- app/Http/Controllers/API/LanguageController.php
+++ app/Http/Controllers/API/LanguageController.php
@@ -10,7 +10,7 @@ class LanguageController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api');
+        $this->middleware('auth:api')->except('functions');
     }
 
     public function index()
